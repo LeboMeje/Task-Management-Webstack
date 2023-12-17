f9e8b245889666d783c85239050cf0b964abe6c6
LeboMeje <lebomhopewell@gmail.com>
2023-12-17 04:47:24 -0800

    adding app components.

---
+++ app.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import TaskList from './components/TaskList';
+
+function App() {
+  return (
+    <div>
+      <h1>Task Management System</h1>
+      <TaskList />
+    </div>
+  );
+}
+
+export default App;
