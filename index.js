06e0382c8fae2cded4dad5d8215471ce1ed38e63
LeboMeje <lebomhopewell@gmail.com>
2023-12-17 05:58:40 -0800

    Adding index file.

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js or App.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { TaskProvider } from './TaskContext';
+
+ReactDOM.render(
+  <TaskProvider>
+    <App />
+  </TaskProvider>,
+  document.getElementById('root')
+);
